57e6ceac896a0e9f83e7dedea24076ffa12a7c50
hya95 <hya95@sfu.ca>
2022-07-18 18:52:14 -0700

    system jump

---
+++ Sprint 2/Application/app/src/main/java/com/example/application/IncomingActivity.java
@@ -0,0 +1,14 @@
+package com.example.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IncomingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.incoming_act_page);
+    }
+}
--- Sprint 2/Application/app/src/main/java/com/example/application/MainActivity.java
+++ Sprint 2/Application/app/src/main/java/com/example/application/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.content.Intent;
@@ -13,24 +14,47 @@ import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
     Button button = null;
-    RadioButton radioButton = null;
+    //RadioButton radioBtn_patient = null;
+    //RadioButton radioBtn_caretaker = null;
+    RadioGroup radioGroup = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
-        radioButton = (RadioButton)findViewById(R.id.Caretaker);
+        radioGroup = findViewById(R.id.radioButton);
         button = (Button) findViewById(R.id.Login_button);
+        //case R.id.Patient:
 
-        button.setOnClickListener(new View.OnClickListener() {
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, CalenderActivity.class);
-                startActivity(intent);
+
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.Patient:
+                        button.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent();
+                                intent.setClass(MainActivity.this, IncomingActivity.class);
+                                startActivity(intent);
+                            }
+                        });
+                        break;
+                    case R.id.Caretaker:
+                        button.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent();
+                                intent.setClass(MainActivity.this, CalenderActivity.class);
+                                startActivity(intent);
+                                Log.i("TAG","case caretaker");
+                            }
+                        });
+                        break;
+
+                }
             }
         });
-
-
     }
-
 }
