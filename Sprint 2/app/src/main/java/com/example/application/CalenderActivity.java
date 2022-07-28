ab9458777418f74ffcb7346e11c7d6654a333c17
zya91 <zya91@sfu.ca>
2022-07-28 16:49:12 -0700

    notification_caregiver

--- Sprint 2/app/src/main/java/com/example/application/CalenderActivity.java
+++ Sprint 2/app/src/main/java/com/example/application/CalenderActivity.java
@@ -1,6 +1,5 @@
 package com.example.application;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
@@ -15,56 +14,43 @@ import android.widget.*;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-//import com.google.firebase.database.FirebaseDatabase;
-import java.util.HashMap;
 
 public class CalenderActivity extends AppCompatActivity{
-    Button add_button = null;
-    Button view_button = null;
-    CalendarView calendar;
-    String day = "";
+    Button button = null;
     //TextView act_msg;
     //TextView tip_msg;
-    // TextView time_msg;
+   // TextView time_msg;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calender_page);
-        add_button = findViewById(R.id.add_button);
-        view_button = findViewById(R.id.view_button);
-        calendar = findViewById(R.id.calendarView);
+        button = (Button) findViewById(R.id.add_button);
+        //button.setBackgroundColor(Color.BLACK);
 
-        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
-                day = i + "-" + (i1 + 1) + "-" + i2;
-                Toast.makeText(CalenderActivity.this, day, Toast.LENGTH_SHORT).show();
-                //FirebaseDatabase.getInstance().getReference().child("Tasks and Dates").child(day).push().setValue(new Task("drink","", 10, 30, 15));
-            }
-        });
 
-        view_button.setOnClickListener(new OnClickListener() {
+        //Intent intent = getIntent();
+        //String str = intent.getStringExtra("message");
+        //String str2 = intent.getStringExtra("message2");
+       // String str3 = intent.getStringExtra("time");
+        //act_msg = (TextView)findViewById(R.id.acticity1);
+
+        //act_msg.setText(str);
+
+        //tip_msg = (TextView)findViewById(R.id.tip);
+        //tip_msg.setText(str2);
+
+
+        //time_msg = (TextView)findViewById(R.id.time);
+        //time_msg.setText(str3.toString());
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-
-                intent.setClass(CalenderActivity.this, EventsList.class);
+                intent.setClass(CalenderActivity.this, addPage.class);
                 startActivity(intent);
-            }
-        });
-        add_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(day.isEmpty()){
-                    Toast.makeText(CalenderActivity.this, "Please tap on a date", Toast.LENGTH_LONG).show();
-                }
-                else {
-                    Intent intent = new Intent();
-                    intent.setClass(CalenderActivity.this, addPage.class);
-                    intent.putExtra("date", day);
-                    startActivity(intent);
-                }
             }
         });
 
--- Sprint 2/app/src/main/java/com/example/application/addPage.java
+++ Sprint 2/app/src/main/java/com/example/application/addPage.java
@@ -2,72 +2,170 @@ package com.example.application;
 
 //import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.os.Build;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.firebase.database.FirebaseDatabase;
+import com.bigkoo.pickerview.builder.TimePickerBuilder;
+import com.bigkoo.pickerview.listener.OnTimeSelectListener;
+import com.example.application.utils.Utils;
+
+import java.util.Date;
 
 public class addPage extends AppCompatActivity {
-
+//    Button button = null;
     Button button_done;
-    Button button_back;
     Button button_camera;
     Button button_galley;
-    EditText send_acts;
+    //TimePicker picker;
+    //TextView time;
+    EditText send_text;
     EditText send_tips;
-    //EditText send_duration;
-    TimePicker time;
+    String mSelectTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_page);
-        initWidget();
+        //time = (TextView)findViewById(R.id.time);
+        //picker = (TimePicker)findViewById(R.id.datePicker1);
+//        picker.setIs24HourView(true);
+
+//        button = (Button) findViewById(R.id.Time_set);
+//        button.setBackgroundColor(Color.parseColor("#F8F8FF"));
+        send_text = findViewById(R.id.text_activity);
+        send_tips = findViewById(R.id.text_tips);
+
+
+        button_done = findViewById(R.id.addDone);
+//        button_camera = findViewById(R.id.);
+//        button_galley = findViewById(R.id.);
+
         button_done.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View v) {
-                String day = getIntent().getStringExtra("date");
-                String activity = send_acts.getText().toString();
-                String tips = send_tips.getText().toString();
-                //String stringDuration = send_duration.getText().toString();
-                int duration;
-                if(activity.isEmpty()){
-                    Toast.makeText(addPage.this, "Info missing!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                int hour = time.getHour();
-                int minute = time.getMinute();
-                Task task = new Task(activity,tips,hour,minute);
-                //Task.taskArrayList.add(task);
-                FirebaseDatabase.getInstance().getReference().child("Tasks and Dates").child(day).push().setValue(task);
+               // int hour, minute;
 
-                Intent intent = new Intent(addPage.this, CalenderActivity.class);
+
+                String str = send_text.getText().toString();
+                String str2 = send_tips.getText().toString();
+                //String am_pm;
+
+
+
+//                if (Build.VERSION.SDK_INT >= 23 ){
+//                    hour = picker.getHour();
+//                    minute = picker.getMinute();
+//                }
+//                else{
+//                    hour = picker.getCurrentHour();
+//                    minute = picker.getCurrentMinute();
+//                }
+//                if(hour > 12) {
+//                    am_pm = "PM";
+//                    hour = hour - 12;
+//                }
+//                else
+//                {
+//                    am_pm="AM";
+//                }
+//                time.setText("Selected Date: "+ hour +":"+ minute+" "+am_pm);
+
+
+                // 插入系统事件
+                Utils.writeToCalendar(addPage.this,str,str2,mSelectTime);
+
+
+                Intent intent = new Intent(getApplicationContext(), CalenderActivity.class);
+                intent.putExtra("message",str);
+                intent.putExtra("message2",str2);
+                //intent.putExtra("time",am_pm);
                 startActivity(intent);
-            }
+                }
         });
 
-        button_back.setOnClickListener(new View.OnClickListener() {
+        final TextView dateTv = findViewById(R.id.datePicker1);
+        dateTv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                Utils.hideSoftKeyboard(addPage.this);
+                new TimePickerBuilder(addPage.this, new OnTimeSelectListener() {
+                    @Override
+                    public void onTimeSelect(Date date, View v) {
+                        mSelectTime = Utils.getDateAndTimeStr(date);
+                        dateTv.setText(mSelectTime);
+                    }
+                }).setType(new boolean[]{false, false, false, true, true, false}) //年月日时分秒 的显示与否，不设置则默认全部显示
+                        .setLabel("年", "月", "日", "", "", "")
+                        .build().show();
             }
         });
 
-    }
 
-    private void initWidget() {
-        button_done = findViewById(R.id.addDone);
-        button_back = findViewById(R.id.back_button);
-        send_acts = findViewById(R.id.set_activity);
-        send_tips = findViewById(R.id.set_tips);
-        //send_duration = findViewById(R.id.duration1);
-        time = findViewById(R.id.timePicker);
-    }
+        //Galley function
+//        button_galley.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v){
+//                Intent intent = new Intent(Intent.ACTION_PICK, MediaStroe.Images.Media.EXTERNAL_CONTENT_URI);
+//                startActivityForResult(intent, 3);
+//            }
+//            });
 
+        //Camera function
+//        button_camera.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                try {
+//                    Intent intent = new Intent();
+//                    intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+//                    startActivity(intent);
+//                }catch (Exception e)
+//                {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+
+
+
+
+//            public void onClick(View v) {
+//                onBackPressed();
+//            }
+//            public void onClick(View v){
+//                int hour, minute;
+//                String am_pm;
+//                if (Build.VERSION.SDK_INT >= 23 ){
+//                    hour = picker.getHour();
+//                    minute = picker.getMinute();
+//                }
+//                else{
+//                    hour = picker.getCurrentHour();
+//                    minute = picker.getCurrentMinute();
+//                }
+//                if(hour > 12) {
+//                    am_pm = "PM";
+//                    hour = hour - 12;
+//                }
+//                else
+//                {
+//                    am_pm="AM";
+//                }
+//                tvw.setText("Selected Date: "+ hour +":"+ minute+" "+am_pm);
+
+
+
+
+
+
+    }
 
 }
