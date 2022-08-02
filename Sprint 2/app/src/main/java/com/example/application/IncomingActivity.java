dabb8b29f09dda2807437dc874172f9ba1134273
Nick Liu <nickliu@Nicks-MacBook-Air.local>
2022-08-01 23:28:55 -0700

    IncomingActivity and Firebase Update

--- Sprint 2/app/src/main/java/com/example/application/IncomingActivity.java
+++ Sprint 2/app/src/main/java/com/example/application/IncomingActivity.java
@@ -1,34 +1,127 @@
 package com.example.application;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.text.SimpleDateFormat;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Date;
+import java.util.Locale;
 
 // Alert Page to display the upcoming task (reminder) when it is close to the time or on time
 public class IncomingActivity extends AppCompatActivity {
-
+    Task currentTask; // The most recent upcoming task
+    String currentDate;
     ImageView button_task_complete; // Button for completing the task
+    TextView activity_detail;
+    TextView tips_detail;
+    TextView time_detail;
+    ImageView task_image;
+    DatabaseReference incompleteTaskReference;
+    String currentTaskKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.incoming_act_page);
-        button_task_complete = (ImageView) findViewById(R.id.button_task_complete);
+        activity_detail = findViewById(R.id.activity_detail);
+        tips_detail = findViewById(R.id.tips_detail);
+        time_detail = findViewById(R.id.time_detail);
+        task_image = findViewById(R.id.task_image);
+        button_task_complete = findViewById(R.id.button_task_complete);
 
-        // When Task Complete Button is clicked
+        // Fetch entry of tasks on current date from FireBase
+        currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+        incompleteTaskReference = FirebaseDatabase.getInstance().getReference("Tasks and Dates").child(currentDate).child("NotComplete");
+        fetchEarliestActivity();
+
+        // Task Complete Button is clicked, user set the task complete manually
         button_task_complete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                // Delete the current Task in "NotComplete"
+                deleteEarliestActivity();
+                // Add the current Task to "Complete"
+                FirebaseDatabase.getInstance().getReference().
+                        child("Tasks and Dates").child(currentDate).child("Complete").
+                        push().setValue(currentTask);
+                if(currentTask != null){
+                    currentTask.clearVariables();
+                    Toast.makeText(IncomingActivity.this,
+                            "Task Completed!", Toast.LENGTH_LONG).show();}
+                // Fetch next earliest task
+                fetchEarliestActivity();
             }
         });
 
     }
+
+    // Method of fetching/pulling the earliest activity from Firebase
+    private void fetchEarliestActivity(){
+        // Sort the timestamp entry and only fetch the one on top
+        incompleteTaskReference.orderByChild("timestamp").limitToFirst(1).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                if (snapshot.getValue() == null || incompleteTaskReference == null){
+                    // child does not exist, meaning no task today
+                    String noActivity = "No Activities Today";
+                    activity_detail.setText(noActivity);
+                }else {
+                    currentTaskKey = snapshot.getKey();
+                    currentTask = snapshot.getValue(Task.class);
+                    assert currentTask != null;
+                    // Update info on screen
+                    activity_detail.setText(currentTask.getTask_name());
+                    tips_detail.setText(currentTask.getTips());
+                    time_detail.setText(currentTask.getTime());
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    // Method of deleting today's earliest incomplete activity in Firebase
+    private void deleteEarliestActivity(){
+        if(currentTaskKey != null) {
+            incompleteTaskReference.child(currentTaskKey).removeValue();
+        }
+    }
+
 }
--- Sprint 2/app/src/main/java/com/example/application/Task.java
+++ Sprint 2/app/src/main/java/com/example/application/Task.java
@@ -10,16 +10,40 @@ public class Task {
     private String task_name;
     private String tips;
     private String time;
-    //private Date delete
-    //public static ArrayList<Task> taskArrayList = new ArrayList<>();
+    private int hour;
+    private int minute;
+    private int timestamp;
+
     public Task() {
     }
+    public Task(String task_name, String tips, int hour, int minute, String time) {
+        this.task_name = task_name;
+        this.tips = tips;
+        this.hour = hour;
+        this.minute = minute;
 
-    public Task(String task_name, String tips, int hour, int minute,String time) {
+        this.time = time; // Store time as String
+        this.timestamp = (int)(hour * 100) + minute; // Store time as integer number
+    }
+
+    public Task(String task_name, String tips, int hour, int minute) {
+        this.task_name = task_name;
+        this.tips = tips;
+        this.hour = hour;
+        this.minute = minute;
+
+        this.time = hour + ":" + minute; // Store time as String
+        this.timestamp = (int)(hour * 100) + minute; // Store time as integer number
+    }
+
+    // Constructor only for displaying text
+    public Task(String task_name, String tips, String time){
         this.task_name = task_name;
         this.tips = tips;
         this.time = time;
     }
+
+
 /*
     public void setHour(int hour) {
         this.hour = hour;
@@ -46,6 +70,24 @@ public class Task {
 
     public String getTips() {
         return tips;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getTimestamp() {
+        return timestamp;
+    }
+
+    public void clearVariables(){
+        this.task_name = "No Activity";
+        this.tips = "";
+        this.time = "";
     }
 
     public void setTask_name(String task_name) {
