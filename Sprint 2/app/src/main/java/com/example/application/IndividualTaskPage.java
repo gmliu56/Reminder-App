a3ecf12ec2d9b2a0d962f169ec91b3ce2fc25760
Nick Liu <nickliu@Nicks-MacBook-Air.local>
2022-08-02 21:34:33 -0700

    minor bug fix

--- Sprint 2/app/src/main/java/com/example/application/IndividualTaskPage.java
+++ Sprint 2/app/src/main/java/com/example/application/IndividualTaskPage.java
@@ -12,17 +12,17 @@ public class IndividualTaskPage extends AppCompatActivity {
     ImageView btn_back_event;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual_task_page);
+
         btn_back_event=findViewById(R.id.back_to_eventList);
         btn_back_event.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent();
-                intent.setClass(IndividualTaskPage.this,EventsList.class);
+                intent.setClass(IndividualTaskPage.this, EventsList.class);
                 startActivity(intent);
             }
         });
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_individual_task_page);
     }
 }
